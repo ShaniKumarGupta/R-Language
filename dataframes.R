f93ac9019d4ac05e2d46c65add2b6d5c76028421
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-13 20:43:30 +0530

    Create data frame and use data.frame as well as view function to explore the dataframe

--- dataframes.R
+++ dataframes.R
@@ -1,1 +1,19 @@
 # Data Frames
+
+# List of data frames present in R by default
+data()
+
+# how to view a particular dataset ?
+View(CO2)
+View(mtcars)
+
+# Lets create our own data frames now
+
+students <- c("Shani","Aman","Vivek","Ritika","Ravi","Lado","Priya")
+weight <- c("60","65","55","60","45","50","52")
+Gender <- c("M","M","M","F","M","M","M")
+
+# lets craete dataframes using these values
+
+df <- data.frame(students,weight,Gender)
+View(df)
