71cf629d3502fe5bfd27c08d6ec97f7ff7b8a494
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-14 23:19:46 +0530

    Create vector and create matrix as well as perfor matrix operations

--- matrices.R
+++ matrices.R
@@ -1,1 +1,29 @@
 # Perform matrix opeartion 
+
+# Lets create a simple vector
+1:10
+2.3:11
+
+# create vector
+c <- 1:10
+b <- 5:14
+
+matrix(c)
+
+v <- c('a','b','c','d','e','f')
+mat <- matrix(v,nrow = 2,ncol = 3, byrow = TRUE)
+print(mat)
+
+# lets learn about nrow and ncol
+
+c<-matrix(c, nrow = 5)
+matrix(c, ncol = 2)
+
+nrow(c)
+print(c)
+
+# Lets name the rows and col
+
+colnames(c) <- c("first","second")
+print(c)
+
