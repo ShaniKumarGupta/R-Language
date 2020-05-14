cb371682af031c142c569b89e21eaa91270868f1
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-14 15:29:24 +0530

    Perform if and ifelse conditional operation

--- conditionandloops.R
+++ conditionandloops.R
@@ -1,1 +1,21 @@
 # Perform conditional and loop task with datasets
+
+#lets start with if condition
+
+check <- 1
+if(check == 1){
+  print("Hello")
+}else{
+  print("Hi")
+}
+
+# ifelse conditions
+
+check <- 2
+ifelse(check==0,"True","False")
+
+# Power of ifelse
+
+testval <- c(1,0,1,0,0,1)
+ifelse(testval == 1,"True","False")
+
