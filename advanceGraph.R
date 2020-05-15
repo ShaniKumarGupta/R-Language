cfb1511c10afdf9c4e68110f39e395b9c66a9a5d
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-16 02:02:58 +0530

    Use Box plot, violin plot as well as corrplots and corrgram

--- advanceGraph.R
+++ advanceGraph.R
@@ -1,1 +1,22 @@
 # Let's learn few advanced graph which we generally use in data analysis
+
+library(ggplot2)
+View(mpg)
+
+plot <- ggplot(mpg, aes(x = factor(cyl), y = displ, fill = factor(cyl)))
+
+# Let's make a box plot
+plot + geom_boxplot()
+
+# violin plot
+
+plot + geom_violin()
+
+# Next up corrplots
+library(corrplot)
+corrplot(cor(mtcars))
+corrplot(cor(mtcars), method = 'square')
+corrplot(cor(mtcars), method = 'number', type = 'upper')
+
+library(corrgram)
+corrgram(cor(mtcars))
