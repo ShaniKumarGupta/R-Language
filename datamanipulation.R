e808b6532d7dbb39327da4104ed8eb68e53816a9
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-15 13:13:04 +0530

    Perform slice, arrange and renaming command

--- datamanipulation.R
+++ datamanipulation.R
@@ -10,3 +10,15 @@ head(db)
 
 # filter from the datasets
 head(filter(db, carat>0.2, cut == "Premium"))
+head(filter(db, carat>0.2, color == "E", price>500))
+
+# suppose we want a particular set of data to be seprated
+df <- slice(db, 20:45)
+print(df)
+
+# Arrange the dataset based on priority of certain parameter
+head(arrange(db, carat, depth, table))
+
+# renaming particular row for appropriate name
+df <- rename(db, Purity = clarity)
+View(df)
