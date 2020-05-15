b98a3b4bf2f7425e4474226b6afc4616d31a638c
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-16 01:32:20 +0530

    Plotting a labeled graph

--- twoContinousVarDataVisual.R
+++ twoContinousVarDataVisual.R
@@ -2,3 +2,15 @@
 # we will use mtcar dataset
 
 library(ggplot2)
+View(mtcars)
+
+plot <- ggplot(mtcars, aes(wt, mpg, label=rownames(mtcars)))
+
+# Let's start with plotting a test graph
+plot + geom_text()
+
+# Let's plot a label graph
+plot + geom_label()
+
+# classify based on cylinders
+plot + geom_label(aes(color = cyl))
