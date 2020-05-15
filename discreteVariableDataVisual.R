8288127fe1eb73e6cedd3442597e72f98621bc65
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-15 22:06:13 +0530

    perform graph ploting to deal with one discrete variable and use bar graph to perform task

---
+++ discreteVariableDataVisual.R
@@ -0,0 +1,30 @@
+# Let's learn how to deal with categorical data or discrete data in x axis
+
+library(ggplot2)
+
+# lets plot bar graph
+
+db <- read.csv("diamonds.csv")
+
+plot <- ggplot(db, aes(x = clarity))
+
+plot + geom_bar()
+
+# count of different clarity of diamonds based on cut
+plot + geom_bar(aes(fill = cut))
+
+# Let's add border to the colors for better understanding
+plot + geom_bar(aes(fill = cut), color = "Black")
+
+# how to distinguish color in terms of persentage
+plot + geom_bar(aes(fill = cut), color = "Black", position = "fill")
+
+plot + geom_bar(aes(fill = cut), color = "Black", position = position_fill(reverse = TRUE))
+
+# other ways to find the values of color in each clarity category
+
+plot + geom_bar(aes(fill = cut), color = "Black", position = "dodge")
+
+plot + geom_bar(aes(fill = cut), color = "Black", position = "stack")
+
+plot + geom_bar(aes(color = color), fill = NA, position = "identity")
