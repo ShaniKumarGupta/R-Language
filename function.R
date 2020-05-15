44223caea7c346c0be655342a0fc557117907d91
ShaniKumarGupta <shani.gupta_cs@outlook.com>
2020-05-15 12:13:42 +0530

    Perform user defind task using one and trwo parameter

--- function.R
+++ function.R
@@ -1,1 +1,25 @@
 # User defind function
+
+# Let's create a simple user defind function
+
+say.hello <- function(){
+  print("hello")
+}
+
+say.hello()
+
+# User defind function with one parameter
+
+say.bye <- function(x){
+  print(sprintf("Hello %s", x))
+}
+
+say.bye("Shani")
+
+# User defind function with two parameter
+
+sum <- function(x,y){
+  print(x+y)
+}
+
+sum(12,23)
